3641009327face533d5636f1365d5bc2a3ae26a5
Sofia Hervéus <sofia@herveus.se>
2021-11-19 05:18:47 +0100

    Delete div-element

--- src/components/SinglePhoto.jsx
+++ src/components/SinglePhoto.jsx
@@ -1,17 +1,15 @@
 function SinglePhoto({ img, flipped }) {
   return (
-    <div>
-      <div className={flipped ? 'flipped' : ''}>
-        <img
-          src={img.src}
-          alt="Photo"
-          className="gallery-list_item-photo front-side "
-        />
-        <div className="gallery-list_item-photo-back back-side">
-          <p>
-            {img.position} - {img.date}
-          </p>
-        </div>
+    <div className={flipped ? 'flipped' : ''}>
+      <img
+        src={img.src}
+        alt="Photo"
+        className="gallery-list_item-photo front-side "
+      />
+      <div className="gallery-list_item-photo-back back-side">
+        <p>
+          {img.position} - {img.date}
+        </p>
       </div>
     </div>
   );
