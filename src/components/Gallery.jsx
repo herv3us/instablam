e1eb435158edb3136a4004144e769745d78f5eae
Sofia Hervéus <sofia@herveus.se>
2021-11-19 06:55:46 +0100

    Fix error with deletefunction

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,42 +1,17 @@
-import { useState } from 'react';
 import SinglePhoto from './SinglePhoto';
 
 function Gallery({ gallery, setGallery }) {
-  const [flipped, setflipped] = useState(true);
-
-  const handleClick = (photo) => {
-    if (flipped) {
-      setflipped(false);
-    } else {
-      setflipped(true);
-    }
-  };
-
   const handleDelete = (id) => {
-    let newGallery = [...gallery];
-    newGallery.map((photo) => {
-      if (photo.id === id) {
-        newGallery.pop(photo);
-        setGallery(newGallery);
-      }
-    });
+    const newGallery = gallery.filter((photo) => photo.id !== id);
+    setGallery(newGallery);
   };
 
   return (
     <div className="gallery">
       <ul className="gallery-list">
         {gallery.map((img) => (
-          <li
-            className="gallery-list_item"
-            key={img.id}
-            onClick={() => handleClick(img.id)}
-          >
-            <SinglePhoto
-              key={img.id}
-              onClick={handleClick}
-              img={img}
-              flipped={flipped}
-            />
+          <li className="gallery-list_item" key={img.id}>
+            <SinglePhoto key={img.id} img={img} />
             <button onClick={() => handleDelete(img.id)}>Delete</button>
           </li>
         ))}
--- src/components/SinglePhoto.jsx
+++ src/components/SinglePhoto.jsx
@@ -1,10 +1,24 @@
-function SinglePhoto({ img, flipped }) {
+import { useState } from 'react';
+
+function SinglePhoto({ img }) {
+  const [flipped, setflipped] = useState(true);
+
+  const handleClick = () => {
+    if (flipped) {
+      setflipped(false);
+    } else {
+      setflipped(true);
+    }
+  };
+
   return (
     <div className={flipped ? 'flipped' : ''}>
       <img
         src={img.src}
         alt="Photo"
         className="gallery-list_item-photo front-side "
+        onClick={handleClick}
+        flipped={flipped}
       />
       <div className="gallery-list_item-photo-back back-side">
         <p>
